0a75a9b9f548201b608de726bb0f739de14f216f
MathiasClari <mathiasclari@noximity.com>
2023-10-04 11:53:47 +0200

    refactor: Moved website to Nextjs 13, Improved performance of the website

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { HiMiniShoppingCart, HiUserPlus } from "react-icons/hi2";
+import logo from "../assets/logos/full/NoximityCompanyLight.svg";
+import styles from "../style/navbar.module.scss";
+
+export default function Navbar() {
+    return (
+        <nav className={styles.nav}>
+            <section className={styles.width}>
+                <div className={styles.left}>
+                    <Image
+                        src={logo}
+                        alt="Noximity"
+                        width={150}
+                        height={35}
+                        priority={true}
+                        quality={100}
+                        className={styles.logo}
+                    />
+                    <a href="/">Home</a>
+                    <a href="https://noximity.studios/">Shop</a>
+                    <a href="/">Explore</a>
+                    <a href="/">Help</a>
+                </div>
+                <div className={styles.right}>
+                    <a href="/">
+                        <HiMiniShoppingCart color={"#b6afdc"} /> Search
+                    </a>
+                    <a href="/">
+                        <HiUserPlus color={"#b6afdc"} /> Login
+                    </a>
+                </div>
+            </section>
+        </nav>
+    );
+}
